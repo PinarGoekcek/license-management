e46fece132b1351d6936673888011bc00cb0f2a1
Andreas Heinrich <anheit07@hs-esslingen.de>
2022-04-30 14:02:43 +0200

    add ExampleTest.java

---
+++ src/test/java/de/hse/gruppe8/ExampleTest.java
@@ -0,0 +1,20 @@
+package de.hse.gruppe8;
+
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.Assert;
+
+@QuarkusTest
+public class ExampleTest {
+
+    @Test
+    void true_is_true() {
+        //given
+
+        //when
+
+        //then
+        Assert.assertTrue(true);
+    }
+}
