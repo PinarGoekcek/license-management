93b6539b82ff8a13d2e339a4904133a7f328166b
Andreas Heinrich <anheit07@hs-esslingen.de>
2022-05-15 16:06:26 +0200

    upgrade quarkus and fix code-coverage in IntelliJ

--- src/main/java/de/hse/gruppe8/util/mapper/MappingConfig.java
+++ src/main/java/de/hse/gruppe8/util/mapper/MappingConfig.java
@@ -2,8 +2,9 @@ package de.hse.gruppe8.util.mapper;
 
 
 import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
 
-@MapperConfig(componentModel = "cdi")
+@MapperConfig(componentModel = "cdi", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MappingConfig {
 
 }
