a3785394ab5415615034b96c038d5469065b70eb
Andreas Heinrich <anheit07@hs-esslingen.de>
2022-05-16 22:01:37 +0200

    add userDao Login test

--- src/test/java/de/hse/gruppe8/orm/UserDaoTest.java
+++ src/test/java/de/hse/gruppe8/orm/UserDaoTest.java
@@ -1,5 +1,6 @@
 package de.hse.gruppe8.orm;
 
+import de.hse.gruppe8.exception.NoUserFoundException;
 import de.hse.gruppe8.orm.dao.UserDao;
 import de.hse.gruppe8.orm.model.UserEntity;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @QuarkusTest
 public class UserDaoTest {
@@ -30,7 +32,7 @@ public class UserDaoTest {
         userDao.save(userEntity);
 
         //Then
-        assertEquals(userDao.getUsers().size(), 1);
+        assertEquals(1, userDao.getUsers().size());
     }
 
     @Test
@@ -46,8 +48,8 @@ public class UserDaoTest {
         userDao.save(user);
 
         //Then
-        assertEquals(userDao.getUsers().size(), 1);
-        assertEquals(userDao.getUser(user.getId()).getFirstName(), newName);
+        assertEquals(1, userDao.getUsers().size());
+        assertEquals(newName, userDao.getUser(user.getId()).getFirstName());
     }
 
     @Test
@@ -60,6 +62,39 @@ public class UserDaoTest {
         userDao.delete(user);
 
         //Then
-        assertEquals(userDao.getUsers().size(), 0);
+        assertEquals(0, userDao.getUsers().size());
+    }
+
+    @Test
+    void checkLoginWithUser() {
+        //Given
+        final String username = "username";
+        final String password = "password";
+        final String firstname = "Firstname";
+        final String lastName = "LastName";
+        UserEntity userEntity = new UserEntity(null, username, password, false, firstname, lastName, "email@admin.de", null, null, true, null);
+        userDao.save(userEntity);
+
+        //When
+        UserEntity user = userDao.login(username, password);
+
+        //Then
+        assertEquals(username, user.getUsername());
+        assertEquals(firstname, user.getFirstName());
+        assertEquals(lastName, user.getLastName());
+    }
+
+    @Test
+    void checkLoginWithoutUser() {
+        //Given
+        final String username = "username";
+        final String password = "password";
+
+        //When && Then
+        NoUserFoundException exception = assertThrows(NoUserFoundException.class, () -> {
+            userDao.login(username, password);
+        });
+
+        assertEquals("Login incorrect. No user with username \"username\" found or password incorrect.", exception.getMessage());
     }
 }
