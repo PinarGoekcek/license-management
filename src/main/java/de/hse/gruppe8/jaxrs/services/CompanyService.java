842e97791345aadf20c8f6a7ad08973686d35dcf
Lilly <ayweit00@hs-esslingen.de>
2022-05-16 15:47:49 +0200

    WIP: add getter to CompanyService

--- src/main/java/de/hse/gruppe8/jaxrs/services/CompanyService.java
+++ src/main/java/de/hse/gruppe8/jaxrs/services/CompanyService.java
@@ -1,10 +1,14 @@
 package de.hse.gruppe8.jaxrs.services;
 
+import de.hse.gruppe8.jaxrs.model.Company;
 import de.hse.gruppe8.orm.dao.CompanyDao;
+import de.hse.gruppe8.orm.model.CompanyEntity;
 import de.hse.gruppe8.util.mapper.CompanyMapper;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class CompanyService {
@@ -14,4 +18,21 @@ public class CompanyService {
 
     @Inject
     CompanyMapper companyMapper;
+
+    public List<Company> getCompanies(){
+        List<CompanyEntity> companyEntities = companyDao.getCompanies();
+        List<Company> companies = new ArrayList<Company>();
+
+        for (int i = 0; i < companyEntities.size(); i++) {
+            companies.add(companyMapper.toCompany(companyEntities.get(i)));
+        }
+
+        return companies;
+    }
+
+    public Company getCompany(Long id){
+        CompanyEntity companyEntity = companyDao.getCompany(id);
+        Company company = companyMapper.toCompany(companyEntity);
+        return company;
+    }
 }
