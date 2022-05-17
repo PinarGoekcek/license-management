e90446a7063da9a0ad8845b6a53e8dd70a0a0ce0
Lilly <ayweit00@hs-esslingen.de>
2022-05-17 14:52:51 +0200

    Uncomment stuff in CompanyMapper

--- src/main/java/de/hse/gruppe8/util/mapper/CompanyMapper.java
+++ src/main/java/de/hse/gruppe8/util/mapper/CompanyMapper.java
@@ -1,10 +1,12 @@
 package de.hse.gruppe8.util.mapper;
 
+import de.hse.gruppe8.jaxrs.model.Company;
+import de.hse.gruppe8.orm.model.CompanyEntity;
 import org.mapstruct.Mapper;
 
 @Mapper(config = MappingConfig.class)
 public interface CompanyMapper {
 
-    // CompanyEntity toCompanyEntity(Company company);
-    // Company toCompany(CompanyEntity companyEntity);
+    CompanyEntity toCompanyEntity(Company company);
+    Company toCompany(CompanyEntity companyEntity);
 }
