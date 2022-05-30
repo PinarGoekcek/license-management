404ac8936e7a25871c115a74c36ba60b36469197
Lilly <ayweit00@hs-esslingen.de>
2022-05-30 23:45:15 +0200

    Use flexbox to create companies table

--- frontend/licence-management-ui/src/components/Companies.js
+++ frontend/licence-management-ui/src/components/Companies.js
@@ -2,23 +2,20 @@ import Company from './Company';
 
 const Companies = ({ companies }) => {
   return (
-    <div className='company pr-4 py-2 '>
-      <table className='table-fixed'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Department</th>
-            <th>Street</th>
-            <th>ZIP</th>
-            <th>City</th>
-            <th>Country</th>
-          </tr>
-        </thead>
-      </table>
+    <>
+      <div className='company flex flex-row justify-between pr-4 py-2 border-b-2'>
+        <span className='tbl-head'>Name</span>
+        <span className='tbl-head'>Department</span>
+        <span className='tbl-head'>Street</span>
+        <span className='tbl-head'>ZIP</span>
+        <span className='tbl-head'>City</span>
+        <span className='tbl-head'>Country</span>
+        <span className='clearfix'></span>
+      </div>
       {companies.map((company) => (
         <Company key={company.id} company={company} />
       ))}
-    </div>
+    </>
   );
 };
 
--- frontend/licence-management-ui/src/components/Company.js
+++ frontend/licence-management-ui/src/components/Company.js
@@ -1,19 +1,15 @@
 const Company = ({ company }) => {
   return (
-    <div className='company pr-4 py-2 border-b-2'>
-      <table>
-        <tbody>
-          <tr>
-            <td>{company.name}</td>
-            <td>{company.department}</td>
-            <td>{company.street}</td>
-            <td>{company.zip_code}</td>
-            <td>{company.city}</td>
-            <td>{company.country}</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <>
+      <div className='company flex flex-row justify-between pr-4 py-2 border-b-2'>
+        <span className='tbl-content'>{company.name}</span>
+        <span className='tbl-content'>{company.department}</span>
+        <span className='tbl-content'>{company.street}</span>
+        <span className='tbl-content'>{company.zip_code}</span>
+        <span className='tbl-content'>{company.city}</span>
+        <span className='tbl-content'>{company.country}</span>
+      </div>
+    </>
   );
 };
 
