b7bdfb3838ff3eb5fa191fb6f39a5fc88b31efa4
Lilly <ayweit00@hs-esslingen.de>
2022-05-31 15:57:45 +0200

    Set routes in App.js, get data for companies from backend

--- frontend/licence-management-ui/src/App.js
+++ frontend/licence-management-ui/src/App.js
@@ -1,48 +1,42 @@
-import { useState } from 'react';
+import {useState} from 'react';
+import {Redirect, Route, Switch, useHistory} from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Companies from './components/Companies';
+import Login from './components/Login';
+import axios from "axios";
+import {routes} from "./config";
 
 function App() {
-  const [companies, setCompanies] = useState([
-    {
-      id: 1,
-      name: 'Company 1',
-      department: 'Sales',
-      street: 'Rosegarden 1',
-      zip_code: '17235',
-      city: 'Flowertown',
-      country: 'Fantasialand',
-      active: true,
-    },
-    {
-      id: 2,
-      name: 'Company 2',
-      department: 'HR',
-      street: 'Sample Street 1',
-      zip_code: 'ZRX651',
-      city: 'Mytown',
-      country: 'Anotherland',
-      active: true,
-    },
-    {
-      id: 3,
-      name: 'Company 3',
-      department: 'Development',
-      street: 'Test Way 99',
-      zip_code: '99997',
-      city: 'Sampletown',
-      country: 'Notrealland',
-      active: true,
-    },
-  ]);
-  return (
-    <div className='container'>
-      <Header title={'Customers'} />
-      <Navbar />
-      <Companies companies={companies} />
-    </div>
-  );
+    const history = useHistory();
+
+    axios.interceptors.response.use((response) => {
+        return response;
+    }, (error) => {
+        if (error.response?.status === 415) {
+            return Promise.reject(error);
+        }
+        history.push(routes.login);
+        return new Promise(() => {
+        });
+    });
+    axios.defaults.withCredentials = true;
+
+    return (
+        <div className='container'>
+            <Header title={'Customers'}/>
+            <Navbar/>
+
+            <Switch>
+                <Route path={routes.companies} exact component={() => <Companies/>}/>
+                <Route path={routes.contracts} exact component={() => <Companies/>}/>
+                <Route path={routes.users} exact component={() => <Companies/>}/>
+                <Route path={routes.login} exact component={() => <Login/>}/>
+
+                <Redirect to={routes.companies}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
