6663756bc1caa4485bdbd35983f440595690821e
Lilly <ayweit00@hs-esslingen.de>
2022-05-31 16:00:03 +0200

    Create axios request in companies, check credentials and handle response

--- frontend/licence-management-ui/src/components/Companies.js
+++ frontend/licence-management-ui/src/components/Companies.js
@@ -1,6 +1,31 @@
+import axios from "axios";
 import Company from './Company';
+import {useState, useEffect } from "react";
+import {APP_API_ENDPOINT_URL, routes} from "../config";
+import {useHistory} from "react-router-dom";
 
-const Companies = ({ companies }) => {
+const Companies = () => {
+  const history = useHistory();
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    let user = JSON.parse(localStorage.getItem("user"))
+    if (user === null) {
+      history.push(routes.login)
+      return
+    }
+    let  jwt = user.jwt || ""
+    axios.get(`${APP_API_ENDPOINT_URL}/companies`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': `Bearer ${jwt}`
+      },
+    }).then((response) => {
+          setCompanies(response.data);
+        })
+  }, []);
+
   return (
     <>
       <div className='company flex flex-row justify-between pr-4 py-2 border-b-2'>
