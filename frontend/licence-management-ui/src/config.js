9ab5405e6ca280df55bc261b9f0a18f551520665
Lilly <ayweit00@hs-esslingen.de>
2022-05-31 15:56:04 +0200

    Add browser-router, setup routes, setup cors

---
+++ frontend/licence-management-ui/src/config.js
@@ -0,0 +1,16 @@
+
+export const protectedRoutes = {
+    'companies': '/companies',
+    'contracts': '/contracts',
+    'users': '/users',
+}
+
+
+export const routes = {
+    ...protectedRoutes,
+    'login': '/login',
+    'logout': '/logout'
+}
+
+
+export const APP_API_ENDPOINT_URL = process.env.REACT_APP_API_ENDPOINT_URL || 'http://localhost:8080';
