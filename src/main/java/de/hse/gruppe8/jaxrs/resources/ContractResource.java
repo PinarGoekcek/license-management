c81ee46551e332c10a402b9b609f9ae4d9636f36
Lilly <ayweit00@hs-esslingen.de>
2022-06-01 19:18:18 +0200

    Add create and get method to ContractResource

--- src/main/java/de/hse/gruppe8/jaxrs/resources/ContractResource.java
+++ src/main/java/de/hse/gruppe8/jaxrs/resources/ContractResource.java
@@ -1,13 +1,26 @@
 package de.hse.gruppe8.jaxrs.resources;
 
+import de.hse.gruppe8.jaxrs.model.Company;
+import de.hse.gruppe8.jaxrs.model.Contract;
+import de.hse.gruppe8.jaxrs.model.ErrorResponse;
+import de.hse.gruppe8.jaxrs.model.User;
+import de.hse.gruppe8.jaxrs.services.ContractService;
+import de.hse.gruppe8.jaxrs.services.UserService;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityRequirement;
 import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 import org.eclipse.microprofile.openapi.annotations.security.SecuritySchemes;
 
+import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.SecurityContext;
 
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
@@ -20,15 +33,38 @@ import javax.ws.rs.core.Response;
 @Path("/contracts")
 public class ContractResource {
 
-    @POST
-    public Response createContract() {
+    @Inject
+    ContractService contractService;
 
-        return null;
+    @Inject
+    UserService userService;
+
+    @POST
+    @APIResponses({
+            @APIResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Contract.class))),
+            @APIResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    public Response createContract(@Context SecurityContext securityContext, Contract contract) {
+        Long user_id = Long.valueOf(securityContext.getUserPrincipal().getName());
+        User currentUser = userService.getUser(user_id);
+        contract.setId(null);
+        contract = contractService.createContract(currentUser, contract);
+        if (contract != null) {
+            return Response.ok().entity(contract).build();
+        } else {
+            return Response.status(401).entity(new ErrorResponse("Can`t create new Contract")).build();
+        }
     }
 
     @GET
-    public Response getContract() {
-        return null;
+    @APIResponses({
+            @APIResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = Contract[].class))),
+            @APIResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
+    public Response getContracts(@Context SecurityContext securityContext) {
+        Long user_id = Long.valueOf(securityContext.getUserPrincipal().getName());
+        User currentUser = userService.getUser(user_id);
+        return Response.ok().entity(contractService.getContracts(currentUser)).build();
     }
 
     @GET
