c9eef9dc0a66a91e2af8123d8ab4d656083d2b84
Lilly <ayweit00@hs-esslingen.de>
2022-06-01 22:20:10 +0200

    Add save to ContractToUserDao, fix getter mathods

--- src/main/java/de/hse/gruppe8/orm/dao/ContractToUserDao.java
+++ src/main/java/de/hse/gruppe8/orm/dao/ContractToUserDao.java
@@ -1,14 +1,16 @@
 package de.hse.gruppe8.orm.dao;
 
 import de.hse.gruppe8.orm.model.ContractEntity;
+import de.hse.gruppe8.orm.model.ContractToUserEntity;
 import de.hse.gruppe8.orm.model.UserEntity;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
@@ -19,16 +21,36 @@ public class ContractToUserDao {
     private static final Logger LOGGER = Logger.getLogger(ContractToUserDao.class);
 
     public List<ContractEntity> getContractsForUser (UserEntity userEntity) {
-        Query q = entityManager.createQuery("select contracts from ContractToUserEntity contracts where contracts.user.id = :USER", UserEntity.class);
+        TypedQuery<ContractToUserEntity> q = entityManager.createQuery("select entities from ContractToUserEntity entities where entities.user.id = :USER", ContractToUserEntity.class);
         q.setParameter("USER", userEntity.getId());
-        List<ContractEntity> contracts = (List<ContractEntity>) q.getResultList();
+        List<ContractToUserEntity> contractUsers = q.getResultList();
+
+        List<ContractEntity> contracts = new ArrayList<>();
+        for (ContractToUserEntity entry : contractUsers){
+            contracts.add(entry.getContract());
+        }
         return contracts;
     }
 
     public List<UserEntity> getUsersForContract (ContractEntity contractEntity) {
-        Query q = entityManager.createQuery("select users from ContractToUserEntity users where users.contract.id = :CONTRACT", ContractEntity.class);
+        TypedQuery<ContractToUserEntity> q = entityManager.createQuery("select entities from ContractToUserEntity entities where entities.contract.id = :CONTRACT", ContractToUserEntity.class);
         q.setParameter("CONTRACT", contractEntity.getId());
-        List<UserEntity> users = (List<UserEntity>) q.getResultList();
+        List<ContractToUserEntity> contractUsers = q.getResultList();
+
+        List<UserEntity> users = new ArrayList<>();
+        for (ContractToUserEntity entry : contractUsers){
+            users.add(entry.getUser());
+        }
         return users;
+    }
+
+    @Transactional
+    public ContractToUserEntity save(ContractToUserEntity contractToUser) {
+        if (contractToUser.getId() != null) {
+            contractToUser = entityManager.merge(contractToUser);
+        } else {
+            entityManager.persist(contractToUser);
+        }
+        return contractToUser;
     }
 }
