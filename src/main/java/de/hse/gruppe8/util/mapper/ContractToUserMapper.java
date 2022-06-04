fbbeca0f026d5be8734a22350953c282ada4f8fa
Lilly <ayweit00@hs-esslingen.de>
2022-06-04 14:18:13 +0200

    Add mapContractToUserEntityWithContractToUser

--- src/main/java/de/hse/gruppe8/util/mapper/ContractToUserMapper.java
+++ src/main/java/de/hse/gruppe8/util/mapper/ContractToUserMapper.java
@@ -1,8 +1,10 @@
 package de.hse.gruppe8.util.mapper;
 
 import de.hse.gruppe8.jaxrs.model.ContractToUser;
+import de.hse.gruppe8.orm.model.CompanyEntity;
 import de.hse.gruppe8.orm.model.ContractToUserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(config = MappingConfig.class, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
@@ -11,4 +13,7 @@ public interface ContractToUserMapper {
     ContractToUserEntity toContractToUserEntity(ContractToUser contractToUser);
 
     ContractToUser toContractToUser(ContractToUserEntity contractToUserEntity);
+
+    ContractToUserEntity mapContractToUserEntityWithContractToUser(@MappingTarget ContractToUserEntity s1, ContractToUser s2);
+
 }
