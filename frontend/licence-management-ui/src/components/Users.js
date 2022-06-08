f701da87c799121edf12b637e886f3c8487c9d63
Lilly <ayweit00@hs-esslingen.de>
2022-06-08 12:33:49 +0200

    Fix users listing

--- frontend/licence-management-ui/src/components/Users.js
+++ frontend/licence-management-ui/src/components/Users.js
@@ -1,54 +1,54 @@
 import axios from "axios";
 import User from './User';
-import {useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import {APP_API_ENDPOINT_URL, routes} from "../config";
 import {useHistory} from "react-router-dom";
 
 const Users = () => {
-  const history = useHistory();
-  const [users, setUsers] = useState([]);
+    const history = useHistory();
+    const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    let user = JSON.parse(localStorage.getItem("user"))
-    if (user === null) {
-      history.push(routes.login)
-      return
-    }
-    let  jwt = user.jwt || ""
-    axios.get(`${APP_API_ENDPOINT_URL}/companies`, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-        'Authorization': `Bearer ${jwt}`
-      },
-    }).then((response) => {
-      setUsers(response.data);
-    })
-  }, []);
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem('user'));
+        if (user === null) {
+            history.push(routes.login);
+            return;
+        }
+        let jwt = user.jwt || '';
+        axios.get(`${APP_API_ENDPOINT_URL}/users`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${jwt}`,
+            },
+        }).then((response) => {
+            setUsers(response.data);
+        });
+    }, []);
 
-  return (
-      <>
-        <div className='user flex flex-row justify-between pr-4 py-2 border-b-2'>
-          <span className='tbl-head'>company_id</span>
-          <span className='tbl-head'>Username</span>
-          <span className='tbl-head'>Password</span>
-          <span className='tbl-head'>Admin</span>
-          <span className='tbl-head'>First name</span>
-          <span className='tbl-head'>Last name</span>
-          <span className='tbl-head'>Email</span>
-          <span className='tbl-head'>Phone</span>
-          <span className='tbl-head'>Mobile</span>
-          <span className='tbl-head'></span>
-          <span className='tbl-head'></span>
-          <span className='tbl-head'></span>
-          <span className='tbl-head'></span>
-          <span className='clearfix'></span>
-        </div>
-        {users.map((user) => (
-            <User key={user.id} user={user} />
-        ))}
-      </>
-  );
+    return (
+        <>
+            <div className='user flex flex-row justify-between pr-4 py-2 border-b-2'>
+                <span className='tbl-head'>Company</span>
+                <span className='tbl-head'>Username</span>
+                <span className='tbl-head'>First name</span>
+                <span className='tbl-head'>Last name</span>
+                <span className='tbl-head'>Email</span>
+                <span className='tbl-head'>Phone</span>
+                <span className='tbl-head'>Mobile</span>
+                <span className='tbl-head'></span>
+                <span className='tbl-head'></span>
+                <span className='clearfix'></span>
+            </div>
+            {users ?
+                users.map((user) => (
+                    <User key={user.id} user={user}/>
+                ))
+                : console.log("No users in list")
+            }
+
+        </>
+    );
 };
 
 export default Users;
