377d02fbfb1e2ba5157b12ebbfa1d88e14deeb4b
Lilly <ayweit00@hs-esslingen.de>
2022-06-08 20:33:27 +0200

    Add missing functions to UserService

--- src/main/java/de/hse/gruppe8/jaxrs/services/UserService.java
+++ src/main/java/de/hse/gruppe8/jaxrs/services/UserService.java
@@ -7,6 +7,8 @@ import de.hse.gruppe8.util.mapper.UserMapper;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 @ApplicationScoped
 public class UserService {
@@ -17,10 +19,47 @@ public class UserService {
     @Inject
     UserMapper userMapper;
 
+    public List<User> getUsers(User currentUser) {
+        List<User> users = new ArrayList<>();
+        if (currentUser.getIsAdmin()) {
+            List<UserEntity> userEntities = userDao.getUsers();
+            for (UserEntity userEntity : userEntities) {
+                users.add(userMapper.toUser(userEntity));
+            }
+        }
+        return users;
+    }
 
     public User getUser(Long id) {
         UserEntity userEntity = userDao.getUser(id);
         return userMapper.toUser(userEntity);
+    }
+
+    public User createUser(User currentUser, User user) {
+        if (currentUser.getIsAdmin()) {
+            UserEntity userEntity = userDao.save(userMapper.toUserEntity(user));
+            return userMapper.toUser(userEntity);
+        }
+        return null;
+    }
+
+    public boolean deleteUser(User currentUser, Long id) {
+        if (currentUser.getIsAdmin()) {
+            userDao.delete(userDao.getUser(id));
+            return true;
+        }
+        return false;
+    }
+
+    public User updateUser(User currentUser, User user) {
+        if (currentUser.getIsAdmin()) {
+            UserEntity userEntity = userDao.getUser(user.getId());
+            userEntity = userMapper.mapUserEntityWithUser(userEntity, user);
+            userEntity = userDao.save(userEntity);
+            return userMapper.toUser(userEntity);
+        } else {
+            return null;
+        }
     }
 
 }
