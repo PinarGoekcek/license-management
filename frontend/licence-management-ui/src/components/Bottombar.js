e9a3f748411ab1daf9404d89e5d4cfa955cd302a
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-08 17:13:44 +0200

    fix Bottombar

---
+++ frontend/licence-management-ui/src/components/Bottombar.js
@@ -0,0 +1,12 @@
+
+const Bottombar = () => {
+    return (
+        <footer className='bottombar absolute bottom-0 w-full py-2 bg-blue-500'>
+            <div className="text-center">
+            <p>Copyright &copy; 2022</p>
+                </div>
+        </footer>
+    );
+};
+
+export default Bottombar;
