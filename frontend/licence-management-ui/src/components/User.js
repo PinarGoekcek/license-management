403dfee803c8a019f0819aaf2babf923e67ec953
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-08 15:35:10 +0200

    fix User buttonTxt + onClick

--- frontend/licence-management-ui/src/components/User.js
+++ frontend/licence-management-ui/src/components/User.js
@@ -1,4 +1,9 @@
+import ButtonTxt from "./ButtonTxt";
+
 const User = ({user}) => {
+    const onClick = () => {
+        console.log('click');
+    };
     return (
         <>
             <div className='user flex flex-row justify-between pr-4 py-2 border-b-2'>
@@ -11,6 +16,12 @@ const User = ({user}) => {
                 <span className='tbl-content'>{user.mobile}</span>
                 <span className='tbl-content'>Edit</span>
                 <span className='tbl-content'>Delete</span>
+                <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
+                <ButtonTxt name={'Edit'} onClick={onClick}/>
+                </span>
+                <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
+                <ButtonTxt name={'Delete'} onClick={onClick}/>
+                </span>
             </div>
         </>
     );
