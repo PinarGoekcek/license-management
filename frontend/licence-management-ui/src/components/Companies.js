937f096a2b442bcd86c73a6aac43d0ab8aa93afe
Lilly <ayweit00@hs-esslingen.de>
2022-06-08 12:34:51 +0200

    Add conditional: only map if companies is not null

--- frontend/licence-management-ui/src/components/Companies.js
+++ frontend/licence-management-ui/src/components/Companies.js
@@ -1,53 +1,55 @@
 import axios from 'axios';
 import Company from './Company';
-import { useState, useEffect } from 'react';
-import { APP_API_ENDPOINT_URL, routes } from '../config';
-import { useHistory } from 'react-router-dom';
+import {useState, useEffect} from 'react';
+import {APP_API_ENDPOINT_URL, routes} from '../config';
+import {useHistory} from 'react-router-dom';
 
 const Companies = () => {
-  const history = useHistory();
-  const [companies, setCompanies] = useState([]);
+    const history = useHistory();
+    const [companies, setCompanies] = useState([]);
 
-  useEffect(() => {
-    let user = JSON.parse(localStorage.getItem('user'));
-    if (user === null) {
-      history.push(routes.login);
-      return;
-    }
-    let jwt = user.jwt || '';
-    axios
-      .get(`${APP_API_ENDPOINT_URL}/companies`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Accept: 'application/json',
-          Authorization: `Bearer ${jwt}`,
-        },
-      })
-      .then((response) => {
-        setCompanies(response.data);
-      });
-  }, []);
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem('user'));
+        if (user === null) {
+            history.push(routes.login);
+            return;
+        }
+        let jwt = user.jwt || '';
+        axios.get(`${APP_API_ENDPOINT_URL}/companies`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${jwt}`,
+            },
+        })
+            .then((response) => {
+                setCompanies(response.data);
+            });
+    }, []);
 
-  return (
-    <>
-      <div className='company border-b-2'>
-        <span className='tbl-row tbl-head'>Name</span>
-        <span className='tbl-row tbl-head'>Department</span>
-        <span className='tbl-row tbl-head'>Street</span>
-        <span className='tbl-row tbl-head'>ZIP</span>
-        <span className='tbl-row tbl-head'>City</span>
-        <span className='tbl-row tbl-head'>Country</span>
-        <span className='tbl-row tbl-head headTxt'></span>
-        <span className='tbl-row tbl-head headTxt'></span>
-        <span className='tbl-row tbl-head headTxt'></span>
-        <span className='tbl-row tbl-head headTxt'></span>
-        <span className='clearfix'></span>
-      </div>
-      {companies.map((company) => (
-        <Company key={company.id} company={company} />
-      ))}
-    </>
-  );
+    return (
+        <>
+            <div className='company border-b-2'>
+                <span className='tbl-row tbl-head'>Name</span>
+                <span className='tbl-row tbl-head'>Department</span>
+                <span className='tbl-row tbl-head'>Street</span>
+                <span className='tbl-row tbl-head'>ZIP</span>
+                <span className='tbl-row tbl-head'>City</span>
+                <span className='tbl-row tbl-head'>Country</span>
+                <span className='tbl-row tbl-head headTxt'></span>
+                <span className='tbl-row tbl-head headTxt'></span>
+                <span className='tbl-row tbl-head headTxt'></span>
+                <span className='tbl-row tbl-head headTxt'></span>
+                <span className='clearfix'></span>
+            </div>
+            {companies ?
+                companies.map((company) => (
+                    <Company key={company.id} company={company}/>
+                ))
+                : console.log("No companies in list")
+            }
+        </>
+    );
 };
 
 export default Companies;
