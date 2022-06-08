29b2e7f1b36e3e9444be88db93321fc8df761423
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-08 14:00:26 +0200

    fix contract listing

--- frontend/licence-management-ui/src/components/Contract.js
+++ frontend/licence-management-ui/src/components/Contract.js
@@ -2,11 +2,10 @@ const Contract = ({ contract }) => {
   return (
     <>
       <div className='contract flex flex-row justify-between pr-4 py-2 border-b-2'>
-        <span className='tbl-content'>{contract.id_company}</span>
-        <span className='tbl-content'>{contract.date_start}</span>
-        <span className='tbl-content'>{contract.date_end}</span>
+        <span className='tbl-content'>{contract.company}</span>
+        <span className='tbl-content'>{contract.dateStart}</span>
+        <span className='tbl-content'>{contract.dateStop}</span>
         <span className='tbl-content'>{contract.version}</span>
-        <span className='tbl-content'>{contract.license_key}</span>
         <span className='tbl-content'>Edit</span>
         <span className='tbl-content'>Delete</span>
         <span className='tbl-content'>Details</span>
