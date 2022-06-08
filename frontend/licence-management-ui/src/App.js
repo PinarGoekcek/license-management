a21808a9df55e59b23274d208840b00ec8865c79
Lilly <ayweit00@hs-esslingen.de>
2022-06-08 11:39:23 +0200

    Fix missing routes and import in Navbar.js, cleanup in App.js

--- frontend/licence-management-ui/src/App.js
+++ frontend/licence-management-ui/src/App.js
@@ -1,47 +1,47 @@
-
 import {Redirect, Route, Switch, useHistory} from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import Companies from './components/Companies';
+import Contracts from "./components/Contracts";
+import Users from "./components/Users";
 import Login from './components/Login';
 
 import axios from "axios";
 import {routes} from "./config";
-import Users from "./components/Users";
-import Contracts from "./components/Contracts";
 
 function App() {
-  const history = useHistory();
+    const history = useHistory();
 
-  axios.interceptors.response.use(
-    (response) => {
-      return response;
-    },
-    (error) => {
-      if (error.response?.status === 415) {
-        return Promise.reject(error);
-      }
-      history.push(routes.login);
-      return new Promise(() => {});
-    }
-  );
-  axios.defaults.withCredentials = true;
+    axios.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if (error.response?.status === 415) {
+                return Promise.reject(error);
+            }
+            history.push(routes.login);
+            return new Promise(() => {
+            });
+        }
+    );
+    axios.defaults.withCredentials = true;
 
-  return (
-    <div className='my-container'>
-      <Header title={'Customers'} />
-      <Navbar />
+    return (
+        <div className='my-container'>
+            <Header title={'Customers'}/>
+            <Navbar/>
 
-      <Switch>
-        <Route path={routes.companies} exact component={() => <Companies />} />
-        <Route path={routes.contracts} exact component={() => <Contracts />} />
-        <Route path={routes.users} exact component={() => <Users />} />
-        <Route path={routes.login} exact component={() => <Login />} />
+            <Switch>
+                <Route path={routes.companies} exact component={() => <Companies/>}/>
+                <Route path={routes.contracts} exact component={() => <Contracts/>}/>
+                <Route path={routes.users} exact component={() => <Users/>}/>
+                <Route path={routes.login} exact component={() => <Login/>}/>
 
-        <Redirect to={routes.companies} />
-      </Switch>
-    </div>
-  );
+                <Redirect to={routes.companies}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
--- frontend/licence-management-ui/src/components/Navbar.js
+++ frontend/licence-management-ui/src/components/Navbar.js
@@ -1,21 +1,23 @@
+import {routes} from "./../config";
+
 const Navbar = () => {
-  return (
-    <nav className='navbar bg-gray-300'>
-      <div className='container'>
-        <ul className='nav'>
-          <li>
-            <a href='#'>Customers</a>
-          </li>
-          <li>
-            <a href={routes.contracts}>Contracts</a>
-          </li>
-          <li>
-            <a href='#'>Users</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+    return (
+        <nav className='navbar bg-gray-300'>
+            <div className='container'>
+                <ul className='nav'>
+                    <li>
+                        <a href={routes.companies}>Customers</a>
+                    </li>
+                    <li>
+                        <a href={routes.contracts}>Contracts</a>
+                    </li>
+                    <li>
+                        <a href={routes.users}>Users</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
