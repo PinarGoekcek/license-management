bc9505b223081c967f8e765f1dbeaef4badf226e
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-11 01:40:07 +0200

    style login

--- frontend/licence-management-ui/src/components/Login.js
+++ frontend/licence-management-ui/src/components/Login.js
@@ -3,6 +3,7 @@ import {APP_API_ENDPOINT_URL, routes} from "../config";
 import {useHistory} from "react-router-dom";
 import {useState} from "react";
 
+
 const Login = (props) => {
     props.func('Login');
 
@@ -39,12 +40,13 @@ const Login = (props) => {
 
 
     return (
-        <div>
-            <h1 className="bg-slate-700 h-14 flex flex-col">Login</h1>
-            <input className="h-14 flex flex-col" type='text' placeholder='User' value={username} onChange={userInput}/>
-            <input className="h-14" type='text' placeholder='Password' value={password} onChange={pwInput}/>
+        <div className="container center mx-auto p-8">
+        <div className="m-12 h-full border border-indigo-800 text-center p-8">
+            <h1 className="h-14 flex flex-col">Login</h1>
+            <input className="place-self-center border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 text-center rounded-md h-14 flex flex-col" type='text' placeholder='User' value={username} onChange={userInput}/>
+            <input className="place-self-center border shadow-sm border-slate-300 focus:outline-none focus:border-sky-500 focus:ring-sky-500 text-center rounded-md h-14" type='text' placeholder='Password' value={password} onChange={pwInput}/>
             <button type='button' onClick={btnLogin}>Login</button>
-        </div>
+        </div></div>
     );
 };
 
