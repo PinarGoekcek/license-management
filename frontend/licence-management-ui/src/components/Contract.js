dc5e0e1a50e5d81303c72ffb7d9676219c0f5290
Lilly <ayweit00@hs-esslingen.de>
2022-06-13 14:35:47 +0200

    Auto stash before merge of "frontend2" and "origin/frontend2"

--- frontend/licence-management-ui/src/components/Contract.js
+++ frontend/licence-management-ui/src/components/Contract.js
@@ -1,28 +1,28 @@
 import ButtonTxt from "./ButtonTxt";
 
 const Contract = ({contract}) => {
-  const onClick = () => {
-    console.log('click');
-  };
-  return (
-    <>
-      <div className='contract flex flex-row justify-between pr-4 py-2 border-b-2'>
-        <span className='tbl-row tbl-content'>{contract.company}</span>
-        <span className='tbl-row tbl-content'>{contract.dateStart}</span>
-        <span className='tbl-row tbl-content'>{contract.dateStop}</span>
-        <span className='tbl-row tbl-content'>{contract.version}</span>
-        <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
+    const onClick = () => {
+        console.log('click');
+    };
+    return (
+        <>
+            <div className='contract flex justify-items-start py-2 border-b-2'>
+                <span className='tbl-row tbl-content'>{contract.company.name}</span>
+                <span className='tbl-row tbl-content'>{contract.dateStart}</span>
+                <span className='tbl-row tbl-content'>{contract.dateStop}</span>
+                <span className='tbl-row tbl-content'>{contract.version}</span>
+                <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
         <ButtonTxt name={'Edit'} onClick={onClick}/>
         </span>
-        <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
+                <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
           <ButtonTxt name={'Delete'} onClick={onClick}/>
         </span>
-        <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
+                <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
           <ButtonTxt name={'Details'} onClick={onClick}/>
         </span>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 };
 
 export default Contract;
