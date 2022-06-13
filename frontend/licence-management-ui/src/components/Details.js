09e8cd5d81701cc92ec633c0ad65a22341d68d3b
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-13 17:10:25 +0200

    add Details

---
+++ frontend/licence-management-ui/src/components/Details.js
@@ -0,0 +1,17 @@
+const Details = (props) => {
+    props.func('Details');
+
+
+
+    return (
+        <>
+
+                <h3 className="">Start</h3>
+
+
+
+        </>
+    );
+};
+
+export default Details;
