720195e3e60d2a50f21fe8cd94a37379426fa472
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-13 17:15:48 +0200

    add button function for Contract

--- frontend/licence-management-ui/src/components/Contract.js
+++ frontend/licence-management-ui/src/components/Contract.js
@@ -1,8 +1,18 @@
 import ButtonTxt from "./ButtonTxt";
+import {routes} from "../config";
+import {useHistory} from "react-router-dom";
 
 const Contract = ({contract}) => {
-    const onClick = () => {
-        console.log('click');
+    const history = useHistory();
+
+    const onEdit = () => {
+        console.log('edit');
+    };
+    const onDelete = () => {
+        console.log('delete');
+    };
+    const onDetails = () => {
+        history.push(routes.details);
     };
     return (
         <>
@@ -12,13 +22,13 @@ const Contract = ({contract}) => {
                 <span className='tbl-row tbl-content'>{contract.dateStop}</span>
                 <span className='tbl-row tbl-content'>{contract.version}</span>
                 <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
-        <ButtonTxt name={'Edit'} onClick={onClick}/>
+        <ButtonTxt name={'Edit'} onClick={onEdit}/>
         </span>
                 <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
-          <ButtonTxt name={'Delete'} onClick={onClick}/>
+          <ButtonTxt name={'Delete'} onClick={onDelete}/>
         </span>
                 <span className='tbl-row tbl-content btnTxt onHoverChangeCol'>
-          <ButtonTxt name={'Details'} onClick={onClick}/>
+          <ButtonTxt name={'Details'} onClick={onDetails}/>
         </span>
             </div>
         </>
