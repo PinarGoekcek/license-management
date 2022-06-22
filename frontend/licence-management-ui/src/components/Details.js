21765c31ca1ebaa6cb957123e6a70470b8bd5661
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-22 22:04:09 +0200

    fix buttons

--- frontend/licence-management-ui/src/components/Details.js
+++ frontend/licence-management-ui/src/components/Details.js
@@ -52,27 +52,11 @@ const Details = (props) => {
     " type='text' placeholder=''/></div>
             </div>
 
-
-            <div className="absolute left-20">
-                <button type='button'
-                        className="bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Update key
-                </button>
-
-                <button type='button'
-                        className="bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Mail key
-                </button>
-            </div>
             <div className="absolute right-20">
-                <button type='button'
-                        className="bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Save
-                </button>
 
                 <button type='button'
                         className="bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Cancel
+                    Close
                 </button>
             </div>
 
