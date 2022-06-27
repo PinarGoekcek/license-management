40f831865a91c8a1e095a50aba77386fc494f0ae
Pinar Goekcek <pigoit00@hs-esslingen.de>
2022-06-27 16:24:36 +0200

    details shows contract data

--- frontend/licence-management-ui/src/components/Details.js
+++ frontend/licence-management-ui/src/components/Details.js
@@ -1,11 +1,44 @@
 import ButtonTxt from "./ButtonTxt";
-import {routes} from "../config";
-import {useHistory} from "react-router-dom";
+import {APP_API_ENDPOINT_URL, routes} from "../config";
+import {useHistory, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const Details = (props) => {
     props.func('Contract details');
     props.showAdd(false);
+
+    const {id} = useParams();
     const history = useHistory();
+
+    const [dateStart] = useState(new Date());
+    const [dateStop] = useState(new Date());
+
+
+
+    const [contract, setContract] = useState([]);
+
+
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem('user'));
+        if (user === null) {
+            history.push(routes.login);
+            return;
+        }
+        let jwt = user.jwt || '';
+        axios.get(`${APP_API_ENDPOINT_URL}/contracts/${id}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Bearer ${jwt}`,
+            },
+        })
+            .then((response) => {
+                setContract(response.data);
+            });
+    }, []);
+
+
     const onClose = () => {
         history.push(routes.contracts)
     };
@@ -13,52 +46,68 @@ const Details = (props) => {
     return (
         <>
             <div className="grid grid-cols-2 p-10 m-auto text-enter ">
+                        <div className="m-3">
+                            <h3 className="text-center">Company</h3>
+                            <input className="block border m-auto text-sm text-slate-500"
+                                   type='text' value={contract&&contract.name} /></div>
+
                 <div className="m-3">
-                    <h3 className="text-center">Start</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/>
+                    <h3 className="text-center">Responsible person 1</h3>
+                    <input className="block border m-auto text-sm text-slate-500"
+                           type='text' placeholder='' value={contract.user1}/></div>
+
+
+                <div className="m-3">
+                    <h3 className="text-center">Responsible person 2</h3>
+                    <input className="block border m-auto text-sm text-slate-500"
+                           type='text' placeholder='' value={contract.user2}/></div>
+
+
+                <div className="m-3">
+                    <h3 className="text-center">Date Start</h3>
+                    <input className="block border m-auto text-sm text-slate-500"
+                           type='date' value={dateStart.toISOString().split('T')[0]}
+                          />
                 </div>
                 <div className="m-3">
-                    <h3 className="text-center">End</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <h3 className="text-center">Date End</h3>
+                    <input className="block border m-auto text-sm text-slate-500"
+                           type='date' value={dateStop.toISOString().split('T')[0]}
+                           /></div>
                 <div className="m-3">
                     <h3 className="text-center">Version</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <input className="block border m-auto text-sm text-slate-500"
+                           type='text' placeholder='' value={contract.version}/></div>
                 <div className="m-3">
-                    <h3 className="text-center">Responsible</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
-                <div className="m-3">
-                    <h3 className="text-center">IP number</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <h3 className="text-center">IP number 1</h3>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder='' value={contract.ipaddress1}
+                          /></div>
                 <div className="m-3">
                     <h3 className="text-center">Feature A</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder=''
+                           value={contract.feature1} /></div>
                 <div className="m-3">
-                    <h3 className="text-center">IP number</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <h3 className="text-center">IP number 2</h3>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder='' value={contract.ipaddress2}
+                        /></div>
                 <div className="m-3">
                     <h3 className="text-center">Feature B</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder=''
+                           value={contract.feature2} /></div>
                 <div className="m-3">
-                    <h3 className="text-center">IP number</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <h3 className="text-center">IP number 3</h3>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder='' value={contract.ipaddress3}
+                           /></div>
                 <div className="m-3">
                     <h3 className="text-center">Feature C</h3>
-                    <input className="block border m-auto text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <input className="block border m-auto text-sm text-slate-500" type='text' placeholder=''
+                           value={contract.feature3} /></div>
                 <div className="m-3">
                     <h3 className="text-center">License key</h3>
-                    <input className="block border m-auto left-30 w-full text-sm text-slate-500
-    " type='text' placeholder=''/></div>
+                    <input className="block border m-auto left-30 w-full text-sm text-slate-500" type='text'
+                           placeholder='' value={contract.licenseKey} /></div>
             </div>
+
 
             <div className="absolute right-20">
         <span className="bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
